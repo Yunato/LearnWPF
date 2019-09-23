9c2f3e3e54b35ca5f38dd3eba2ff5b0c679afa1c
Yunato <yunato525@gmail.com>
2019-09-23 22:29:25 +0900

    03 View Model MVVM Basics

---
+++ WpfTreeView/WpfTreeView/Class1.cs
@@ -0,0 +1,44 @@
+﻿//using PropertyChanged;
+//using System.ComponentModel;
+
+//namespace WpfTreeView
+//{
+//    [ImplementPropertyChanged]
+//    public class Class1 : INotifyPropertyChanged
+//    {
+//        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+//        public string Test { get; set; }
+//        //public string Test
+//        //{
+//        //    get 
+//        //    {
+//        //        return mTest;
+//        //    }
+//        //    set 
+//        //    {
+//        //        if (mTest == value)
+//        //            return;
+
+//        //        mTest = value;
+//        //         ハードコーディングでは"名前の変更"でプロパティ名」を変更したとき、同時に変更されないため nameof() を呼ぶ
+//        //        PropertyChanged(this, new PropertyChangedEventArgs(nameof(Test)));
+//        //    }
+//        //}
+
+//        //public Class1()
+//        //{
+//        //    Task.Run(async () =>
+//        //    {
+//        //        int i = 0;
+
+//        //        while (true)
+//        //        {
+//        //            await Task.Delay(200);
+//        //            Test = (i++).ToString();
+
+//        //        }
+//        //    });
+//        //}
+//    }
+//}
---
+++ WpfTreeView/WpfTreeView/Directory/ViewModels/DirectoryStructureViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// The view model for the application main Directory view
+    /// </summary>
+    public class DirectoryStructureViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// A list of all directories on the machine
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Items { get; set; }
+
+        #endregion
+
+        #region
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DirectoryStructureViewModel()
+        {
+            // Get the logical drives
+            var children = DirectoryStructure.GetLogicalDrive();
+
+            // Create the view models from data
+            this.Items = new ObservableCollection<DirectoryItemViewModel>(
+                children.Select(drive => new DirectoryItemViewModel(drive.FullPath, DirectoryItemType.Drive)));
+        }
+
+        #endregion
+    }
+}
